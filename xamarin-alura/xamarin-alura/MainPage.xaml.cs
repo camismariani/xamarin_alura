d87a4f52a42f5e16a25ad7696dfadcdbc61a3fcd
Camila <camilamariani.cm@gmail.com>
2023-11-06 23:25:38 -0300

    ajuste do repositório remoto

---
+++ xamarin-alura/xamarin-alura/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin_alura
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+

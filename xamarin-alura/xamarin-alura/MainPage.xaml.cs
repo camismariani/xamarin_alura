3aa97a5fe9de2331cc01eccdee30329148472771
Camila <camilamariani.cm@gmail.com>
2023-11-07 14:27:39 -0300

    uso de ListView e Binding

--- xamarin-alura/xamarin-alura/MainPage.xaml.cs
+++ xamarin-alura/xamarin-alura/MainPage.xaml.cs
@@ -8,11 +8,41 @@ using Xamarin.Forms;
 
 namespace xamarin_alura
 {
+
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        
+
+    }
+
     public partial class MainPage : ContentPage
     {
+
+        public List<Veiculo> Veiculos { get; set; }    
         public MainPage()
         {
             InitializeComponent();
+
+            this.Veiculos = new List<Veiculo>
+            {
+                new Veiculo {Nome="Gol Bolinha", Preco=35000},
+                new Veiculo { Nome="Variante",Preco=15000},
+                new Veiculo {Nome="Golf",Preco=55000},
+                new Veiculo {Nome="Onix",Preco=80000}
+            };
+
+            this.BindingContext = this;
+
+           // listViewVeiculos.ItemsSource = this.Veiculos;
+
+           // listViewVeiculos.ItemsSource = new string[]
+           // {
+           //     "Gol Bolinha",
+           //     "Variante",
+           //     "Jeep",
+           // };
         }
     }
 }
